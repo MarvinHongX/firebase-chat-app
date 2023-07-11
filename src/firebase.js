def685704cbb29d9de83a0b8ba9a58326310dffb
Marvin Hong <marvinhongx@gmail.com>
2023-07-11 15:25:10 +0900

    first

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import 'firebase/storage'
 
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAUYR_QHy1KXsXCEIrdskKkEY2Z8-fASMQ',
-  authDomain: 'fir-chat-app-40988.firebaseapp.com',
-  databaseURL: 'https://fir-chat-app-40988-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'fir-chat-app-40988',
-  storageBucket: 'fir-chat-app-40988.appspot.com',
-  messagingSenderId: '1096781445050',
-  appId: '1:1096781445050:web:5c8932ff931e1f18950f83',
-  measurementId: 'G-E0WP3F5EX3'
+
 }
 
 const app = initializeApp(firebaseConfig)
